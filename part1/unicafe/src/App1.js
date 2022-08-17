218f663b26c869c58937faf3ce751b8aecdccdda
DESKTOP-JO9CG28\migue <miguel_omar.ec@hotmail.com>
2022-08-17 13:59:13 +0100

    useStateApps1y2

---
+++ part1/unicafe/src/App1.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+const App1 = () => {
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () => {
+    const newClicks = {                  // Asignacion de una variable al controlador el evento para actualizar el estado
+      ...clicks, 
+      good: clicks.good + 1
+    }
+    setClicks(newClicks)
+  }
+
+  const handleNeutralClick = () => {
+    setClicks({ ...clicks, neutral: clicks.neutral + 1 })             // No es necesario asignar una variable, asi es mas simple
+  }
+
+  const handleBadClick = () => {
+    setClicks({ ...clicks, bad: clicks.bad + 1 })
+  }
+
+  const handleClickReset = () => {
+    setClicks({good:0, neutral:0, bad:0})
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+
+      <div>
+          <button onClick={handleClickReset}>Reset</button>
+      </div>
+
+      <h3>Statistics</h3>
+        <p>Good {clicks.good}</p>
+        <p>Neutral {clicks.neutral}</p>
+        <p>Bad {clicks.bad}</p>
+        <p>All {clicks.good + clicks.neutral + clicks.bad}</p>
+        <p>Average {(clicks.good-clicks.bad) / (clicks.good + clicks.neutral + clicks.bad)}</p>
+        <p>Positive {(clicks.good / (clicks.good + clicks.neutral + clicks.bad))*100}%</p>
+    </div>
+  )
+}
+
+export default App1
---
+++ part1/unicafe/src/App2.js
@@ -0,0 +1,57 @@
+// Usando varios estados simples
+
+import React, { useState } from 'react'
+
+const App2 = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat('G'))
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat('B'))
+    setBad(bad + 1)
+  }
+
+  const handleClickReset = () => {
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
+    setAll([])
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+
+      <div>
+        <button onClick={handleClickReset}>Reset</button>
+      </div>
+
+      <h3>Statistics</h3>
+      <p>{allClicks.join(', ')}</p>      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {good + neutral + bad}</p>
+      <p>Average {(good-bad) / (good + neutral + bad)}</p>
+      <p>Positive {(good / (good + neutral + bad))*100}%</p>
+    </div>
+  )
+}
+
+export default App2
